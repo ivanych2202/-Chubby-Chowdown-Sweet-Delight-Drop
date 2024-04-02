1573048426ac10bb3592b52a4f806fb867ca29e1
ivanych2202 <ivanych2202@yandex.ru>
2024-04-02 13:29:18 +0300

    Initial Commit

---
+++ Chubby Chowdown Sweet Delight Drop/Chubby_Chowdown_Sweet_Delight_DropApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chubby_Chowdown_Sweet_Delight_DropApp.swift
+//  Chubby Chowdown Sweet Delight Drop
+//
+//  Created by Ivan Elonov on 02.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Chubby_Chowdown_Sweet_Delight_DropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
